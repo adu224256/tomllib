59fcc1883eef78356013b63545b6c297d0ad8a6f
adu224256 <99267460+adu224256@users.noreply.github.com>
2023-07-29 13:56:09 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="tomllib",
+    version="0.1.0",
+    description="Allows for simple disk writing to keep persistent data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adu224256/tomllib",
+    author="Sauce",
+    author_email="saucejullyfish@gmail.com",
+    license="GNU",
+    packages=["tomllib"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["tomlkit"],
+)
---
+++ tomllib/toml.py
@@ -0,0 +1,163 @@
+import tomlkit
+
+
+class TomlConfig:
+    def __init__(self, data, filename=None):
+        """
+        Class initializes, not called directly.
+        """
+        for key, value in data.items():
+            if isinstance(value, dict):
+                setattr(self, key, TomlConfig(value))
+            else:
+                setattr(self, key, value)
+        self.__file__ = filename
+
+    @classmethod
+    def load(cls, filename: str):
+        """
+        Load TOML data from the specified file and create a new instance of TomlConfig.
+
+        Args:
+            filename (str): The path of the TOML file to load.
+
+        Returns:
+            TomlConfig: An instance of TomlConfig containing the data from the file.
+
+        Example:
+            toml = TomlConfig.load('file.toml')
+        """
+
+        with open(filename, "a", encoding='utf-8'):
+            pass
+        with open(filename, "r", encoding='utf-8') as file:
+            data = tomlkit.loads(file.read())
+        return cls(data, filename=filename)
+
+    def add_item(self, key: str, value):
+        """
+        Add a new item to the TOML data.
+
+        Args:
+            key (str): The key of the item to add, can be nested using dot notation.
+            value (Any): The value of the item to add.
+
+        Example:
+            toml.add_item('section1.key1', 'value1')
+        """
+        keys = key.split('.')
+        current_level = self
+        for idx, k in enumerate(keys):
+            if not hasattr(current_level, k):
+                if idx == len(keys) - 1:
+                    setattr(current_level, k, value)
+                else:
+                    setattr(current_level, k, TomlConfig({}))
+            else:
+                if idx == len(keys) - 1:
+                    setattr(current_level, k, value)
+            current_level = getattr(current_level, k)
+
+    def remove_item(self, key: str):
+        """
+        Remove an item from the TOML data.
+
+        This method allows you to remove an item (key-value pair) from the TOML data.
+
+        Args:
+            key (str): The key of the item to remove.
+
+        Example:
+            toml.remove_item('section1.key1')
+        """
+        keys = key.split('.')
+        current_level = self
+        for idx, k in enumerate(keys):
+            if not hasattr(current_level, k):
+                raise KeyError(f"Key '{key}' does not exist.")
+            if idx == len(keys) - 1:
+                delattr(current_level, k)
+                break
+            current_level = getattr(current_level, k)
+
+    def update_item(self, key: str, value):
+        """
+        Update the value of an existing item in the TOML data.
+
+        This method allows you to update the value of an existing item in the TOML data.
+
+        Args:
+            key (str): The key of the item to update.
+            value (Any): The new value to set for the item.
+
+        Raises:
+            KeyError: If the specified key does not exist.
+
+        Example:
+            toml.update_item('section1.key1', 'new_value')
+        """
+        if hasattr(self, key):
+            setattr(self, key, value)
+        else:
+            raise KeyError(f"Key '{key}' does not exist.")
+
+    def to_dict(self):
+        data = {}
+        for key, value in self.__dict__.items():
+            if key == '__file__' or value is None:
+                continue
+            elif isinstance(value, TomlConfig):
+                data[key] = value.to_dict()
+            else:
+                data[key] = value
+        return data
+
+    def save_to_file(self, filename):
+        """
+        Save TOML to a new file
+
+        Args:
+            filename (str): path to the TOML file
+
+        Example:
+            toml.save_to_file('filepath')
+        """
+        data = self.to_dict()
+
+        with open(filename, "w", encoding='utf-8') as file:
+            file.write(tomlkit.dumps(data))
+
+    def add_items(self, items: list[dict]):
+        """
+        Add multiple items to the TOML data.
+
+        This method allows you to add multiple key-value pairs to the TOML data at once.
+        The items parameter should be a list of dictionaries, where each dictionary represents
+        a key-value pair. Nested keys can be specified using dot notation.
+
+        Args:
+            items (list[dict]): A list of dictionaries representing the items to add.
+
+        Example:
+            toml.add_items([
+                {"section1.key1": "value1"},
+                {"section2.key2": "value2"},
+                {"section3.key3": "value3"},
+            ])
+        """
+        for item in items:
+            for key, value in item.items():
+                self.add_item(key, value)
+
+    def save(self):
+        """
+        Save the modified TOML data back to the original file.
+
+        Example:
+            toml.save()
+        """
+
+        if self.__file__ is None:
+            raise ValueError(
+                "Original filename not available. Use save_to_file() instead.")
+        self.save_to_file(self.__file__)
